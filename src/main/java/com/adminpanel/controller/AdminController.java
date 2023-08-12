e9eda34d1f34d73dc667119ec0c7870b82efd9a4
Nithin-Pramod <nithinpramod1521@gmail.com>
2023-08-13 04:59:21 +0530

    first commit

---
+++ src/main/java/com/adminpanel/controller/AdminController.java
@@ -0,0 +1,93 @@
+package com.adminpanel.controller;
+
+import com.adminpanel.dto.UserRegistrationDto;
+import com.adminpanel.model.User;
+import com.adminpanel.repository.UserRepository;
+import com.adminpanel.service.RoleService;
+import com.adminpanel.service.RoleServiceImpl;
+import com.adminpanel.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class AdminController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Autowired
+    private RoleService roleService;
+
+
+    @GetMapping("/listUsers")
+    public String showAllUsers(Model model){
+        List<User> users = userRepository.findAll();
+        model.addAttribute("listUsers",users);
+        return "listUser";
+    }
+
+    @GetMapping("/addUsers")
+    public String AddUser(Model model){
+        User user = new User();
+        model.addAttribute("user",user);
+        return "addUser";
+
+    }
+
+    @PostMapping("/saveUser")
+    public String saveEmployee(@ModelAttribute("user") UserRegistrationDto user) {
+        // save employee to database
+        userService.save(user);
+        return "redirect:/listUsers";
+    }
+
+    @GetMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable(value = "id") long id) {
+
+        // call delete employee method
+        this.userService.deleteUserById(id);
+        return "redirect:/listUsers";
+    }
+
+    @GetMapping("/addAdmin")
+    public String showUsers(Model model){
+        List<User> users = userRepository.findAll();
+        model.addAttribute("listUsers",users);
+        return "addAdmin";
+    }
+
+    @GetMapping("/makeAdmin/{id}")
+    public String makeUserAdmin(@PathVariable("id") Long userId) {
+        roleService.updateRoleNameById(userId,"ROLE_ADMIN");
+
+        return "redirect:/addAdmin";
+    }
+
+    @GetMapping("/updateUser/{id}")
+    public String updateUserForm(@PathVariable("id") Long userId, Model model) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + userId));
+
+        model.addAttribute("user", user);
+        model.addAttribute("userId", userId); // Add the userId to be used in the form action
+        return "updateUser";
+    }
+
+    @PostMapping("/updateUser/{id}")
+    public String updateUser(@PathVariable("id") Long userId, @ModelAttribute("user") UserRegistrationDto updatedUser) {
+        userService.updateUser(userId, updatedUser);
+
+        return "redirect:/listUsers"; // Redirect with success parameter
+    }
+
+}
---
+++ src/main/java/com/adminpanel/service/RoleServiceImpl.java
@@ -0,0 +1,22 @@
+package com.adminpanel.service;
+
+import com.adminpanel.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RoleServiceImpl implements RoleService{
+
+    private  RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Transactional
+    public void updateRoleNameById(Long roleId, String newName) {
+        roleRepository.updateRoleNameById(roleId, newName);
+    }
+}
